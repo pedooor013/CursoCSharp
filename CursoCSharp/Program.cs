1b17a16dc812d8c67cfc610627bc707bc0831d24
Pedro Lopes <pedrolopes.programador013@gmail.com>
2025-09-02 21:05:05 -0300

    fixx: adicionando comentarios para entender o que cada linha faz

--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -1,11 +1,11 @@
-﻿using System;
+﻿using System; // Parecido com o #include <stdio.h> do C, ele fornece comandos basicos para o C#
 using System.Globalization;
 
-namespace CursoCSharp
+namespace CursoCSharp //Cada function deve estar dentro de um namespace, para que assim seja mantido o padrao do C#
 {
-    internal class Program
+    internal class Program //Para que o codigo seja executado em C# ele deve estar dentro de uma CLASS, ela deve contem o mesmo nome do arquivo C#
     {
-        static void Main(string[] args)
+        static void Main(string[] args) //Apos essa operacao o C# entende que deve iniciar a compilacao do codigo
         {
             Console.WriteLine("Hello World!");
         }

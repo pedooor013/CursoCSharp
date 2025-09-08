1935f27c3aea2eedd95cdcca9d7f4bb11ebcf001
Pedro Lopes <pedrolopes.programador013@gmail.com>
2025-09-08 17:04:43 -0300

    addd: exercicio de soma

---
+++ Soma/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Soma;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int num1, num2, result;
+
+        Console.WriteLine("Digite o valor de um numero: ");     
+        num1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Digite o valor de um numero: ");
+        num2 = int.Parse(Console.ReadLine());
+
+        result = num1 + num2;
+        
+        Console.WriteLine($"SOMA = {result}");
+    }
+
+}

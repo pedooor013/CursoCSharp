b5116329e5e25a884caeffc707283780b069f385
Pedro Lopes <pedrolopes.programador013@gmail.com>
2025-09-08 16:20:27 -0300

    addd: aplicacao do uso da funcao split em um array C#

--- ExercicioEntradaDeDados/Program.cs
+++ ExercicioEntradaDeDados/Program.cs
@@ -7,8 +7,14 @@ class Program
     static void Main(string[] args)
     {
         string nomeUser;
+        Console.WriteLine("Digite o seu nome inteiro: ");
+        nomeUser = Console.ReadLine();
         int qntQuartos;
+        Console.WriteLine("Digite a quantidade de quartos da sua casa: ");
+        qntQuartos = int.Parse(Console.ReadLine());
         double precoProduto;
+        Console.WriteLine("Digite o preço de um produto: ");
+        precoProduto = double.Parse(Console.ReadLine());
         Console.WriteLine("Digite na mesma linha o seu nome, idade, altura");
         string[] infoUser = Console.ReadLine().Split(' ');
         string ultimoNome = infoUser[0];
@@ -16,14 +22,9 @@ class Program
         float alturaUser = float.Parse(infoUser[2]);
         
         //Imprimir no console as instruções
-        Console.WriteLine("Digite o seu nome inteiro: ");
-        nomeUser = Console.ReadLine();
-        Console.WriteLine("Digite a quantidade de quartos da sua casa: ");
-         qntQuartos = int.Parse(Console.ReadLine());
-        Console.WriteLine("Digite o preço de um produto: ");
-        precoProduto = double.Parse(Console.ReadLine());
 
         //Imprimir dados digitados 
+        Console.WriteLine("Itens digitados: ");
         Console.WriteLine(nomeUser);
         Console.WriteLine(qntQuartos);
         Console.WriteLine(precoProduto);

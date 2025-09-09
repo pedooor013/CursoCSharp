693844fff4ca138637f913dad3beb29d27b0b178
Pedro Lopes <pedrolopes.programador013@gmail.com>
2025-09-09 16:09:57 -0300

    addd: exercicio para calculo do raio de um circulo

---
+++ RaioCirculo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace RaioCirculo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double raio, area, pi = 3.14159;
+
+        Console.WriteLine("Digite o valor do raio");
+        raio = double.Parse(Console.ReadLine());
+
+        area = (raio*raio) * pi;
+
+        Console.WriteLine($"A area de um circulo com raio de {raio} eh igual a {area}");
+
+
+    }
+} 
